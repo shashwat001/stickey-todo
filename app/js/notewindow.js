b7e20b1b9661eb1d35dccb39662ad41543b1bcf6
Shashwat Kumar <shashwatkmr.001@gmail.com>
2017-05-05 03:40:21 +0530

    Editable working but not getting clicked in middle of text

--- app/js/notewindow.js
+++ app/js/notewindow.js
@@ -7,7 +7,38 @@ function notewindow()
 }
 
 function createNote() {
-	return $('<div>').addClass('draggablenote');
+	var html = '<ul id="myUL">';
+	html += '<li>test content</li>';
+	html += '</ul>';
+	var node = $('<div>').addClass('draggablenote').html(html);
+	
+	var myNodelist = node[0].getElementsByTagName("LI");
+	var i;
+	for (i = 0; i < myNodelist.length; i++) {
+	  var span = document.createElement("SPAN");
+	  var txt = document.createTextNode("\u00D7");
+	  span.className = "close";
+	  span.appendChild(txt);
+	  myNodelist[i].appendChild(span);
+	  myNodelist[i].onclick = function()
+	  {
+		  console.log(this);
+		  $(this).focus();
+		  this.setAttribute('contenteditable', true);
+	  }
+	}
+
+	// Click on a close button to hide the current list item
+	var close = node[0].getElementsByClassName("close");
+	var i;
+	for (i = 0; i < close.length; i++) {
+	  close[i].onclick = function() {
+	    var div = this.parentElement;
+	    div.style.display = "none";
+	  }
+	}
+	
+	return node;
 }
 
 module.exports = notewindow;
---
+++ app/js/script.js
@@ -0,0 +1,7 @@
+var notewindow = require('./js/notewindow');
+$('document').ready(function() {
+	$('#plus').click(function() {
+		$('#notes').append(new notewindow().node);
+	});
+
+});
