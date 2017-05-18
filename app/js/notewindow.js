e8ca463d7d51dff93d1c234ca5bbf01b8313fccb
Shashwat Kumar <shashwatkmr.001@gmail.com>
2017-05-18 16:59:09 +0530

    Todo app data loading successfully

--- app/js/notewindow.js
+++ app/js/notewindow.js
@@ -1,22 +1,29 @@
-function notewindow()
+function notewindow(isempty)
 {
-	this.node = createNote();
+	this.node = createNote(isempty);
 	this.node.draggable({
 		containment : "parent"
 	});
 	this.node.dblclick(function(){
 		$(this).draggable({ disabled: false });
+		$(this).css( 'cursor', 'move' );
 	});
 	this.node.click(function(){
 		$(this).draggable({ disabled: true });
+		$(this).css( 'cursor', 'text' );
 	});
 }
 
-function createNote() {
+function createNote(isempty) {
 	var $span = $('<span>').addClass('title').attr('contentEditable', true);
-	var $ul = $('<ul>').addClass('myUL').append(getLi());
+	var node = $('<div>').addClass('draggablenote').append($span);
 
-	var node = $('<div>').addClass('draggablenote').append($span).append($ul);
+	if(!isempty)
+	{
+		var $ul = $('<ul>').addClass('myUL').append(getLi());
+		node = node.append($ul);
+	}
+
 	return node;
 
 
--- main.js
+++ main.js
@@ -9,16 +9,29 @@ const {
 const electronLocalshortcut = require('electron-localshortcut');
 
 app.on('ready', createWindow);
+app.on('will-quit', cleanUp);
+app.on('window-all-closed', function(){
+    app.quit();
+});
 //app.on('before-quit', saveData);
 
 let mainWindow;
 
 function createWindow(){
-	mainWindow = new BrowserWindow({width: 800, height:600});
+	mainWindow = new BrowserWindow();
 	mainWindow.loadURL('file://' + __dirname + '/app/main.html');
 	
 	electronLocalshortcut.register(mainWindow,'ctrl+S', () => {
-	    mainWindow.webContents.send('global-shortcut', 'saveData');
+	    mainWindow.webContents.send('saveData');
+	  }) 
+	electronLocalshortcut.register(mainWindow,'ctrl+T', () => {
+	    mainWindow.webContents.send('open-new-note');
 	  }) 
 	mainWindow.webContents.openDevTools();
+}
+
+
+function cleanUp()
+{
+	electronLocalshortcut.unregisterAll();
 }
